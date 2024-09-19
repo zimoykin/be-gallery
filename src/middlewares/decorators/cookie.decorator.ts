ff379c8d8d14d7a3b0a49e3cc5d87732ebe4a625
Dima <zimoykin@gmail.com>
2024-09-19 15:33:55 +0200

    added debug log

--- src/middlewares/decorators/cookie.decorator.ts
+++ src/middlewares/decorators/cookie.decorator.ts
@@ -1,8 +1,10 @@
 
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { cookieProfileAuth } from '../profile-auth.middleware';
+import { sign } from 'crypto';
 
 export const Profile = createParamDecorator((_, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+    console.log({ signedCookies: request.signedCookies });
     return request.signedCookies[cookieProfileAuth];
 });
