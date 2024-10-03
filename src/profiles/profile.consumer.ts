3bdfb68381db49eb3979af0ad27e272a13a6cad8
Dima <zimoykin@gmail.com>
2024-10-03 22:28:20 +0200

    added consumer + creating account

---
+++ src/profiles/profile.consumer.ts
@@ -0,0 +1,33 @@
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+import { AmqpConsumer, InjectConsumer } from "@zimoykin/amqp";
+import { Profile } from "./models/profile.model";
+import { ProfileService } from "./profile.service";
+
+type ProfileDto = {
+    name: string;
+    email: string;
+    id: string;
+    role: 'user' | 'admin';
+};
+
+@Injectable()
+export class ProfileConsumer implements OnModuleInit {
+    private readonly logger = new Logger(ProfileConsumer.name);
+
+    constructor(
+        private readonly profileService: ProfileService,
+        @InjectConsumer(Profile.name) private readonly consumer: AmqpConsumer
+    ) { }
+
+    onModuleInit() {
+        this.consumer.subscribe<ProfileDto>(async msg => {
+            this.logger.debug(JSON.stringify(msg));
+
+            const profile = this.profileService.findProfileByUserId(msg.id);
+            if (!profile) {
+                this.logger.debug('profile not found, creating');
+                await this.profileService.createProfile(msg.id, msg.name, msg.email);
+            }
+        });
+    }
+}
